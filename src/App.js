b58d7d78003456a7af77ad24adf0e9f46af3728f
Ajay <ajay.ks@morrisonsplc.co.uk>
2021-10-12 13:57:43 +0530

    3rdCommit

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ import Login from './components/Login';
 function App() {
   return (
     <div className="App">
+      <div><h1>Hii Login using your google account</h1></div>
       <Login/>
     </div>
   );
